df48f29442a05355cbbd4482fd4e9c74cdc409d9
scxue <xueshuchen17@mails.ucas.edu.cn>
2025-07-22 06:19:06 +0000

    Deploying to gh-pages from @ scxue/scxue.github.io@920cfe2cccb02c11bc7f3cd309ed6d694772e557 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -514,7 +514,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%75%65%73%68%75%63%68%65%6E%31%37@%6D%61%69%6C%73.%75%63%61%73.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -535,7 +535,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=SrFAcEkAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
